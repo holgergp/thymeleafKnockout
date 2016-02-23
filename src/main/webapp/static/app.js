4e4b451befe21e50a341d3ee84de54d20afc221d
Holger Grosse-Plankermann <hgp@Holgers-MacBook-Pro-2.local>
2016-02-23 09:21:43 +0100

    slidesVisible as computed function

--- src/main/webapp/static/app.js
+++ src/main/webapp/static/app.js
@@ -5,53 +5,55 @@
 
 
 $(function () {
-    var viewModel = {
-        burger: ko.observableArray([]),
-        selectedBurger: ko.observable(),// Nothing selected by default
-        sides: ko.observableArray([]),
-        selectedSide: ko.observable(),// Nothing selected by default
-        sidesVisible: ko.observable(false),
-        drinks: ko.observableArray([]),
-        selectedDrink: ko.observable(),// Nothing selected by default
-        drinksVisible: ko.observable(false)
-    };
 
-
-    viewModel.selectedBurger.subscribe(function (newValue) {
-        console.log(newValue);
-        viewModel.sidesVisible(true);
-    });
-
-    viewModel.sidesVisible.subscribe(function (newValue) {
-        if (viewModel.sidesVisible()) {
-            $.getJSON("/sides", function (sidesBean) {
-                console.log(sidesBean);
-
-
-                sidesBean.sidesList.forEach(function (entry) {
-                    viewModel.sides.push(entry);
-                });
-                // Now use this data to update your view models,
-                // and Knockout will update your UI automatically
-            });
-        }
-    });
-
-
-    ko.applyBindings(viewModel, $('#knockoutRegion')[0]);
-
-    $.getJSON("/burger", function (burgerBean) {
-        console.log(burgerBean);
-
-
-        burgerBean.burgerEntryList.forEach(function (entry) {
-            viewModel.burger.push(entry);
+    function viewModel() {
+        var self = this;
+        self.burger = ko.observableArray([]);
+        self.selectedBurger = ko.observable();// Nothing selected by default
+        self.sides = ko.observableArray([]);
+        self.selectedSide = ko.observable();// Nothing selected by default
+        self.sidesVisible = ko.computed(function () {
+            return !s.isBlank(self.selectedBurger());
         });
-        // Now use this data to update your view models,
-        // and Knockout will update your UI automatically
-    });
+        self.drinks = ko.observableArray([]);
+        self.selectedDrink = ko.observable();// Nothing selected by default
+        self.drinksVisible = ko.observable(false);
 
 
+        self.selectedBurger.subscribe(function (newValue) {
+            console.log(newValue);
+        });
+
+        self.sidesVisible.subscribe(function (newValue) {
+            if (self.sidesVisible()) {
+                $.getJSON("/sides", function (sidesBean) {
+                    console.log(sidesBean);
+
+
+                    sidesBean.sidesList.forEach(function (entry) {
+                        self.sides.push(entry);
+                    });
+                    // Now use this data to update your view models,
+                    // and Knockout will update your UI automatically
+                });
+            }
+        });
+
+
+        $.getJSON("/burger", function (burgerBean) {
+            console.log(burgerBean);
+
+
+            burgerBean.burgerEntryList.forEach(function (entry) {
+                self.burger.push(entry);
+            });
+            // Now use this data to update your view models,
+            // and Knockout will update your UI automatically
+        });
+
+    }
+
+    ko.applyBindings(new viewModel(), $('#knockoutRegion')[0]);
 });
 
 
